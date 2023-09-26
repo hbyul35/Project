2e932e9e135b7708283c8a88da02002389050809
HanByeolChoi <80669633+hbyul35@users.noreply.github.com>
2023-09-26 15:37:14 +0900

    Update NutrientController.cs

--- Data/FoodData/NutrientController.cs
+++ Data/FoodData/NutrientController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NutrientController : MonoBehaviour
 {
@@ -9,13 +10,6 @@ public class NutrientController : MonoBehaviour
     public float sugar = 0f;                                              // 당류
     public float dietaryFiber = 0f;                                       // 식이섬유
     public float vitamins = 0f;                                           // 비타민
-
-    GameObject Gcarbohydrates;
-    GameObject Gprotein;
-    GameObject Gfats;
-    GameObject Gsugar;
-    GameObject GdietaryFiber;
-    GameObject Gvitamins;
 
     // 영양소 표시용 UI 텍스트 등
     public TextMesh nutritionText; // UI 텍스트 오브젝트에 연결
@@ -35,7 +29,6 @@ public class NutrientController : MonoBehaviour
         sugar = 0f;
         dietaryFiber = 0f;
         vitamins = 0f;
-
     }
 
     public void UpdateNutrients(float carb, float prot, float fat, float sugar, float fiber, float vit)
@@ -50,25 +43,4 @@ public class NutrientController : MonoBehaviour
         // 여기서 영양소 변화에 따른 처리를 할 수 있음
         // 예: UI 업데이트, 영양소 상태 체크, 게임 진행 상태에 영향 주기 등
     }
-
-    //// 영양소 섭취
-    //public void ConsumeNutrition()
-    //{
-
-    //}
-
-
-    //ui에 영양소 정보 출력
-    //private void updatenutritiontext()
-    //{
-    //    if (nutritiontext != null)
-    //    {
-    //        nutritiontext.text = "carbs: " + carbohydrates + "\n" +
-    //                             "protein: " + protein + "\n" +
-    //                             "fat: " + fat + "\n" +
-    //                             "sugar: " + sugar + "\n" +
-    //                             "fiber: " + dietaryfiber + "\n" +
-    //                             "vitamins: " + vitamins;
-    //    }
-    //}
 }
