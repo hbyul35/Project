60008fa5c72c221adca75bb0e13ec7eae4ea6e11
HanByeolChoi <80669633+hbyul35@users.noreply.github.com>
2023-09-26 15:29:16 +0900

    Create NutrientController.cs

---
+++ Data/FoodData/NutrientController.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+
+public class NutrientController : MonoBehaviour
+{
+    // 플레이어의 영양소 상태
+    public float carbohydrates = 0f;                                      // 탄수화물
+    public float protein = 0f;                                            // 단백질
+    public float fats = 0f;                                               // 지방
+    public float sugar = 0f;                                              // 당류
+    public float dietaryFiber = 0f;                                       // 식이섬유
+    public float vitamins = 0f;                                           // 비타민
+
+    GameObject Gcarbohydrates;
+    GameObject Gprotein;
+    GameObject Gfats;
+    GameObject Gsugar;
+    GameObject GdietaryFiber;
+    GameObject Gvitamins;
+
+    // 영양소 표시용 UI 텍스트 등
+    public TextMesh nutritionText; // UI 텍스트 오브젝트에 연결
+
+    private void Start()
+    {
+        // 게임 시작 시 초기 영양소 값을 설정합니다.
+        ResetNutrition();
+    }
+
+    // 영양소 초기화
+    public void ResetNutrition()
+    {
+        carbohydrates = 0f;
+        protein = 0f;
+        fats = 0f;
+        sugar = 0f;
+        dietaryFiber = 0f;
+        vitamins = 0f;
+
+    }
+
+    public void UpdateNutrients(float carb, float prot, float fat, float sugar, float fiber, float vit)
+    {
+        carbohydrates += carb;
+        protein += prot;
+        this.fats += fat;
+        this.sugar += sugar;
+        dietaryFiber += fiber;
+        vitamins += vit;
+
+        // 여기서 영양소 변화에 따른 처리를 할 수 있음
+        // 예: UI 업데이트, 영양소 상태 체크, 게임 진행 상태에 영향 주기 등
+    }
+
+    //// 영양소 섭취
+    //public void ConsumeNutrition()
+    //{
+
+    //}
+
+
+    //ui에 영양소 정보 출력
+    //private void updatenutritiontext()
+    //{
+    //    if (nutritiontext != null)
+    //    {
+    //        nutritiontext.text = "carbs: " + carbohydrates + "\n" +
+    //                             "protein: " + protein + "\n" +
+    //                             "fat: " + fat + "\n" +
+    //                             "sugar: " + sugar + "\n" +
+    //                             "fiber: " + dietaryfiber + "\n" +
+    //                             "vitamins: " + vitamins;
+    //    }
+    //}
+}
