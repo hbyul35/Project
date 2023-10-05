d935653d9b62bd29c7f0b9b92dabb882f18056ac
Restyled.io <commits@restyled.io>
2023-10-05 02:07:02 +0000

    Restyled by clang-format

--- Eit_it_Project_ver_09.26/Assets/Scripts/CubeController.cs
+++ Eit_it_Project_ver_09.26/Assets/Scripts/CubeController.cs
@@ -3,94 +3,84 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class CubeController : MonoBehaviour
-{
-    public float raycastDistance = 0.3f;
-    public float pushPullSpeed = 2.0f;
+public class CubeController : MonoBehaviour {
+  public float raycastDistance = 0.3f;
+  public float pushPullSpeed = 2.0f;
 
-    private GameObject targetCube;
+  private GameObject targetCube;
 
-    private bool isMoving = false;
-    private bool isRotating;
+  private bool isMoving = false;
+  private bool isRotating;
 
-    void Start()
-    {
+  void Start() {}
 
-    }
+  void Update() {
+    isRotating =
+        transform.parent.GetComponent<PlayerMoveController>().isRotating;
 
+    Ray ray = new Ray(transform.position, transform.forward);
+    RaycastHit hit;
 
-    void Update()
-    {
-        isRotating = transform.parent.GetComponent<PlayerMoveController>().isRotating;
+    if (Physics.Raycast(ray, out hit, raycastDistance)) {
+      Debug.DrawRay(transform.position, transform.forward * raycastDistance,
+                    Color.red);
 
-        Ray ray = new Ray(transform.position, transform.forward);
-        RaycastHit hit;
+      if (hit.collider.CompareTag("Cube")) {
+        targetCube = hit.collider.gameObject;
 
-        if (Physics.Raycast(ray, out hit, raycastDistance))
-        {
-            Debug.DrawRay(transform.position, transform.forward * raycastDistance, Color.red);
-
-            if (hit.collider.CompareTag("Cube"))
-            {
-                targetCube = hit.collider.gameObject;
-
-                if (Input.GetKey(KeyCode.W) && Input.GetKeyDown(KeyCode.Space) && !isMoving && !isRotating)
-                {
-                    StartCoroutine(pushingCube(targetCube));
-                }
-                else if (Input.GetKeyDown(KeyCode.Space) && !isMoving &&!isRotating )
-                {
-                    StartCoroutine(pullingCube(targetCube));
-                }
-            }
+        if (Input.GetKey(KeyCode.W) && Input.GetKeyDown(KeyCode.Space) &&
+            !isMoving && !isRotating) {
+          StartCoroutine(pushingCube(targetCube));
+        } else if (Input.GetKeyDown(KeyCode.Space) && !isMoving &&
+                   !isRotating) {
+          StartCoroutine(pullingCube(targetCube));
         }
+      }
+    }
+  }
+
+  IEnumerator pullingCube(GameObject cubeToMove) {
+    isMoving = true;
+
+    Vector3 start = cubeToMove.transform.position;
+    Vector3 end = start - transform.forward * cubeToMove.transform.localScale.z;
+    float journeyLength = Vector3.Distance(
+        start, end); // public static float Distance(Vector3 a, Vector3 b);
+
+    float startTime = Time.time;
+    float journeyDuration = journeyLength / pushPullSpeed;
+
+    while (Time.time - startTime < journeyDuration) {
+      float distanceCovered = (Time.time - startTime) * pushPullSpeed;
+      float fractionOfJourney = distanceCovered / journeyLength;
+      cubeToMove.transform.position =
+          Vector3.Lerp(start, end, fractionOfJourney);
+      yield return null;
     }
 
+    cubeToMove.transform.position = end;
+    isMoving = false;
+  }
 
-    IEnumerator pullingCube(GameObject cubeToMove)
-    {
-        isMoving = true;
+  IEnumerator pushingCube(GameObject cubeToMove) {
+    isMoving = true;
 
-        Vector3 start = cubeToMove.transform.position;
-        Vector3 end = start - transform.forward * cubeToMove.transform.localScale.z;
-        float journeyLength = Vector3.Distance(start, end); // public static float Distance(Vector3 a, Vector3 b);
+    Vector3 start = cubeToMove.transform.position;
+    Vector3 end = start + transform.forward * cubeToMove.transform.localScale.z;
+    float journeyLength = Vector3.Distance(start, end);
 
-        float startTime = Time.time;
-        float journeyDuration = journeyLength / pushPullSpeed;
+    float startTime = Time.time;
+    float journeyDuration = journeyLength / pushPullSpeed;
 
-        while (Time.time - startTime < journeyDuration)
-        {
-            float distanceCovered = (Time.time - startTime) * pushPullSpeed;
-            float fractionOfJourney = distanceCovered / journeyLength;
-            cubeToMove.transform.position = Vector3.Lerp(start, end, fractionOfJourney);
-            yield return null;
-        }
-
-        cubeToMove.transform.position = end;
-        isMoving = false;
+    while (Time.time - startTime < journeyDuration) {
+      float distanceCovered = (Time.time - startTime) * pushPullSpeed;
+      float fractionOfJourney = distanceCovered / journeyLength;
+      cubeToMove.transform.position =
+          Vector3.Lerp(start, end, fractionOfJourney);
+      yield return null;
     }
 
-
-    IEnumerator pushingCube(GameObject cubeToMove)
-    {
-        isMoving = true;
-
-        Vector3 start = cubeToMove.transform.position;
-        Vector3 end = start + transform.forward * cubeToMove.transform.localScale.z;
-        float journeyLength = Vector3.Distance(start, end);
-
-        float startTime = Time.time;
-        float journeyDuration = journeyLength / pushPullSpeed;
-
-        while (Time.time - startTime < journeyDuration)
-        {
-            float distanceCovered = (Time.time - startTime) * pushPullSpeed;
-            float fractionOfJourney = distanceCovered / journeyLength;
-            cubeToMove.transform.position = Vector3.Lerp(start, end, fractionOfJourney);
-            yield return null;
-        }
-
-        cubeToMove.transform.position = end;
-        isMoving = false;
-    }
+    cubeToMove.transform.position = end;
+    isMoving = false;
+  }
 }
--- Eit_it_Project_ver_10.05/Assets/Scripts/HeadCollisionCheck.cs
+++ Eit_it_Project_ver_10.05/Assets/Scripts/HeadCollisionCheck.cs
@@ -2,33 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HeadCollisionCheck : MonoBehaviour
-{
-    public bool isColliding = false;
-    void Start()
-    {
+public class HeadCollisionCheck : MonoBehaviour {
+  public bool isColliding = false;
+  void Start() {}
 
+  // Update is called once per frame
+  void Update() {}
+
+  void OnTriggerEnter(Collider other) {
+    if (other.gameObject.CompareTag("Cube")) {
+      isColliding = true;
     }
+  }
 
-    // Update is called once per frame
-    void Update()
-    {
-
+  void OnTriggerExit(Collider other) {
+    if (other.gameObject.CompareTag("Cube")) {
+      isColliding = false;
     }
-
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.CompareTag("Cube"))
-        {
-            isColliding = true;
-        }
-    }
-
-    void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.CompareTag("Cube"))
-        {
-            isColliding = false;
-        }
-    }
+  }
 }
