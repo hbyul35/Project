0e6ffae89e0adb9e951b1b4a7f0315b4526cb525
KyeongHo <k45212825@gmail.com>
2023-11-20 03:07:54 +0900

    Eit_it_Project_ver_11.20
    - 업데이트 사항 -
    채공상태 행동제어
    플레이어 움직임&인터랙션 코드 리팩토링
    게임오버 및 재시작
    추가

---
+++ Eit_it_Project_ver_11.20/Assets/Scripts/Scene/MySceneManager.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MySceneManager : MonoBehaviour
+{
+    public ScrollRect gameTutorialsInstance;
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+
+    }
+
+    public void GameExit()
+    {
+        Application.Quit();
+    }
+
+    public void UserInformationSceneLoad()
+    {
+        SceneManager.LoadScene("UserInformationScene");
+    }
+
+    public void CharacterSelectSceneLoad()
+    {
+        GameManager.Instance.Calculate_BMI_And_Recommend(); 
+
+        if (GameManager.Instance.Flag)
+        {           
+            SceneManager.LoadScene("CharacterSelectScene");
+            GameManager.Instance.Flag = false; 
+        }        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("player"))
+        {
+            SceneManager.LoadScene("ScoreScene");
+            PlayerPrefs.SetInt("Scene", 1); // Information for the score scene 
+        }      
+    }
+    public void SwitchingToCharacterLoadScene()
+    {
+        SceneManager.LoadScene("CharacterSelectScene");
+    }
+
+    public void GameTutorialsUI()
+    {
+        gameTutorialsInstance.gameObject.SetActive(true);
+    }
+    
+}
